78e49b2294160c05a9990fea9fe87a34fa8b321b
Simon Baumgartner <me@sens3.com>
2012-06-29 16:47:40 -0400

    Adding README, fixing run_instruments.sh

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kraut Example app
+
+#####Disclaimer
+This app was built going through [this tutorial](http://www.raywenderlich.com/5138/beginning-storyboards-in-ios-5-part-1) by Ray Wenderlich.
+
+##### How to run
+	git clone git@github.com:sens3/kraut-example.git
+	cd kraut-example
+	git submodule update --init --recursive
+	cd Ratings/UIAutomationTests/
+	./run_instruments.sh
--- Ratings/UIAutomationTests/run_instruments.sh
+++ Ratings/UIAutomationTests/run_instruments.sh
@@ -1,2 +1,4 @@
 
-instruments -t "/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate" /Users/smn/prog/iphone/Ratings/DerivedData/Ratings/Build/Products/Debug-iphonesimulator/Ratings.app -e UIASCRIPT /Users/smn/prog/iphone/Ratings/Ratings/UIAutomationTests/js/main.js
+currentDir=$(pwd)
+
+instruments -t "/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate" $currentDir/../../DerivedData/Ratings/Build/Products/Debug-iphonesimulator/Ratings.app -e UIASCRIPT $currentDir/js/main.js
