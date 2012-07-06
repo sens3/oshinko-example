bf4834410a5e1daf9abcd5ff1b2ee6a066b3ef6f
Simon Baumgartner <me@sens3.com>
2012-07-06 12:52:54 -0400

    updated oshinko, now comes with bash script files

--- Ratings/UIAutomationTests/js/main.js
+++ Ratings/UIAutomationTests/js/main.js
@@ -2,13 +2,8 @@
 // Load Oshinko
 #import "oshinko/oshinko.js"
 
-// Load your step definitions (if you have any)
+// load your step definitions, if you have any
 #import "step_definitions/mine.js"
 
-// set the path to your features directory
-
-Oshinko.init({
-  featureDir: "Ratings/UIAutomationTests/features"
-});
-
-Oshinko.run(); 
+// Run Oshinko
+Oshinko.run();
